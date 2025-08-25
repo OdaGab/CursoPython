a6d17d05352c5ed26521aae6820d84da57674718
Odair <odavita32@gmail.com>
2025-08-25 15:01:54 -0300

    Declarando váriaveis

---
+++ Variáveis/nome_das_variaveis.py
@@ -0,0 +1,51 @@
+
+a = 10                #sim 
+forca = 100           #sim     
+forca100 = 1000       #sim  
+forca_normal = 10000  #sim  
+# forca local = 20     não 
+forca10valendo = 200  #sim 
+ÇÇÇ = 300             #sim   
+até = 400             #sim  
+_Odair = 500          #sim  
+#1a = 600             #não
+
+print(a, forca, forca100, forca_normal, forca10valendo, ÇÇÇ, até, _Odair, 1A)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
